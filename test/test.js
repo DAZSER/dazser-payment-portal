48b0af68b7e2a2a7fef1b2dc5f8fa428552941aa
Kyle McNally <kyle@kmcnally.net>
2020-03-25 11:10:29 -0400

    Fix tests

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 const expect = require("chai").expect;
 
-const fee = require("../fee").default;
+const fee = require("../dist/fee").default;
 
 describe("fee.ts", function () {
   describe("default()", function () {
-    it("should return 0's for everything", function() {
+    it("should return 0's for everything", function () {
       const results = fee("0");
       expect(results.total).to.eq("0");
     });
-    it("should return fee amounts", function (){
+    it("should return fee amounts", function () {
       const results = fee("100");
       expect(results.total).to.eq("10330");
       expect(results.fee).to.eq("330");
