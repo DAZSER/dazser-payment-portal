003d2bbda186bbd08672855c9a708df6166b3c3a
Kyle McNally <kyle@kmcnally.net>
2021-04-05 15:42:10 -0400

    Add webhook

--- typescript/backend/charge.ts
+++ typescript/backend/charge.ts
@@ -1,5 +1,6 @@
 // Backend, lol (it serves the frontend)
 // This is the server side renderer
+import Big from "big.js";
 import { urlencoded, json } from "body-parser";
 import compression from "compression";
 import crypto from "crypto";
@@ -11,6 +12,7 @@ import { join } from "path";
 import favicon from "serve-favicon";
 import serverless from "serverless-http";
 import Stripe from "stripe";
+import SQS from "aws-sdk/clients/sqs";
 import calculateFee from "../fee";
 
 interface InvoicePayload {
@@ -30,6 +32,62 @@ interface FrontEndForm {
   email: string;
   invoice: string;
   totalAmount: string;
+}
+
+interface ChargeSucceeded {
+  amount: number;
+  // eslint-disable-next-line camelcase
+  client_reference_id: string;
+  created: number;
+  id: string;
+  // eslint-disable-next-line camelcase
+  payment_method_details: {
+    card: {
+      brand: string;
+      // eslint-disable-next-line camelcase
+      exp_month: number;
+      // eslint-disable-next-line camelcase
+      exp_year: number;
+      last4: string;
+    };
+  };
+  // eslint-disable-next-line camelcase
+  receipt_email: string;
+  status: string;
+}
+
+interface EmailPayload {
+  attachments?: string; // This is a path to S3 for the attachment
+  bcc?: string | string[];
+  body: string; // This is the inner body of the message
+  calendar?: string; // This is an iCal string
+  cc?: string | string[];
+  dsn?: boolean;
+  from: {
+    address: string;
+    name?: string;
+  };
+  invoice?: {
+    file: {
+      // This is the S3 Invoice PDF
+      content: Buffer;
+      filename: string;
+    };
+    info: string;
+    invoice: string;
+  };
+  performance?: {
+    // This is only included in the Performance Payload
+    custName: string;
+    header: string;
+    serviceAddress: string;
+    unique: string;
+  };
+  preview?: string;
+  regionnum: string; // This gives me the regionnum so I can get regionInfo
+  subject: string;
+  template: "notify.html"; // This is the template to merge with
+  to: string;
 }
 
 const getStripePrivateKey = (city: string): string => {
@@ -109,6 +167,23 @@ const parseInfo = (info: string): InvoicePayload => {
   }
 };
 
+const notify = async (payload: EmailPayload): Promise<boolean> => {
+  const sqs = new SQS({
+    region: "us-east-1",
+  });
+  try {
+    await sqs
+      .sendMessage({
+        MessageBody: JSON.stringify(payload),
+        QueueUrl: process.env.SQS_QUEUE as string,
+      })
+      .promise();
+    return true;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
 // This defines the nonce used in the Script nonce tag for CSP purposes.
 const nonce = crypto.randomBytes(16).toString("hex");
 
@@ -168,6 +243,65 @@ app.get("/success", (_request: Express.Request, response: Express.Response) => {
   // This path is for outdated browsers
   response.status(200).render("success", { nonce });
 });
+
+app.post(
+  "/webhook/:city",
+  (request: Express.Request, response: Express.Response) => {
+    // This deals with the web hook from Stripe
+    const payload = request.body as Stripe.Event;
+
+    // get region num
+    const stripeKey = getStripePublicKey(request.params.city);
+
+    let body = "";
+    if (payload.type === "charge.succeeded") {
+      // Create a notify payload for sqs
+      const charge = payload.data.object as ChargeSucceeded;
+      const amount = new Big(charge.amount)
+        .times(new Big(0.971))
+        .div(new Big(100));
+      const created = new Date(charge.created);
+      body = `<table style="width:100%;" border="1">
+        <tr><td colspan="2">A charge has succeeded.</td></tr>
+        <tr><td>Email</td><td>${charge.receipt_email}</td></tr>
+        <tr><td>Invoice</td><td>${charge.client_reference_id ?? ""}</td></tr>
+        <tr><td>Amount</td><td>${amount.round(2).toString()}</td></tr>
+        <tr><td>Charged on</td><td>${created.toLocaleDateString(
+          "en-US"
+        )}</td></tr>
+        <tr><td>Card Brand</td><td>${
+          charge.payment_method_details.card.brand
+        }</td></tr>
+        <tr><td>Card Expiration</td><td>${`${charge.payment_method_details.card.exp_month}/${charge.payment_method_details.card.exp_year}`}</td></tr>
+        <tr><td>Card Last Four</td><td>${
+          charge.payment_method_details.card.last4
+        }</td></tr>
+        <tr><td>Charge ID</td><td>${charge.id}</td></tr>
+        <tr><td>Charge Status</td><td>${charge.status}</td></tr>
+        </table>`;
+    }
+
+    notify({
+      body,
+      from: { address: "network.admin@dazser.com", name: "Payment Portal" },
+      regionnum: stripeKey.regionNumber,
+      subject: "Payment Notification",
+      template: "notify.html",
+      to:
+        "sanderson@dazser.com, chiki.bodley@dazser.com, jade.dato@dazser.com, collections.assistant@dazser.com",
+    })
+      .then((success) => {
+        // eslint-disable-next-line promise/always-return
+        if (success) {
+          response.send(200);
+        }
+      })
+      .catch((error) => {
+        console.error("Stripe Webhook Error", error);
+        response.send(500);
+      });
+  }
+);
 
 app.post(
   "/createCheckoutSession/:city",
