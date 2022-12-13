b5ef884a96e6d1f752571a90f591f7b7f3e73fb9
Sparticuz <kyle@kmcnally.net>
2022-12-13 16:24:34 -0500

    Linting for node 18

--- typescript/backend/get-stripe-keys.ts
+++ typescript/backend/get-stripe-keys.ts
@@ -16,25 +16,25 @@ export const getStripePrivateKey = (city: string): stripePrivateKey => {
     case "tampa":
       return {
         regionNumber: "1",
-        stripePrivateKey: process.env.STRIPE_TAMPA_PRIVATE_KEY as string,
+        stripePrivateKey: process.env["STRIPE_TAMPA_PRIVATE_KEY"] as string,
       };
     /* istanbul ignore next */
     case "orlando":
       return {
         regionNumber: "2",
-        stripePrivateKey: process.env.STRIPE_ORLANDO_PRIVATE_KEY as string,
+        stripePrivateKey: process.env["STRIPE_ORLANDO_PRIVATE_KEY"] as string,
       };
     /* istanbul ignore next */
     case "birmingham":
       return {
         regionNumber: "3",
-        stripePrivateKey: process.env.STRIPE_BIRMINGHAM_PRIVATE_KEY as string,
+        stripePrivateKey: process.env["STRIPE_BIRMINGHAM_PRIVATE_KEY"] as string,
       };
     /* istanbul ignore next */
     case "baltimore":
       return {
         regionNumber: "4",
-        stripePrivateKey: process.env.STRIPE_BALTIMORE_PRIVATE_KEY as string,
+        stripePrivateKey: process.env["STRIPE_BALTIMORE_PRIVATE_KEY"] as string,
       };
     default:
       return {
@@ -50,25 +50,25 @@ export const getStripePublicKey = (city: string): stripePublicKey => {
       return {
         cityName: "Jani-King of Baltimore",
         regionNumber: "4",
-        stripePublicKey: process.env.STRIPE_BALTIMORE_PUBLIC_KEY as string,
+        stripePublicKey: process.env["STRIPE_BALTIMORE_PUBLIC_KEY"] as string,
       };
     case "birmingham":
       return {
         cityName: "Jani-King of Birmingham",
         regionNumber: "3",
-        stripePublicKey: process.env.STRIPE_BIRMINGHAM_PUBLIC_KEY as string,
+        stripePublicKey: process.env["STRIPE_BIRMINGHAM_PUBLIC_KEY"] as string,
       };
     case "orlando":
       return {
         cityName: "Jani-King of Orlando",
         regionNumber: "2",
-        stripePublicKey: process.env.STRIPE_ORLANDO_PUBLIC_KEY as string,
+        stripePublicKey: process.env["STRIPE_ORLANDO_PUBLIC_KEY"] as string,
       };
     case "tampa":
       return {
         cityName: "Jani-King of Tampa Bay",
         regionNumber: "1",
-        stripePublicKey: process.env.STRIPE_TAMPA_PUBLIC_KEY as string,
+        stripePublicKey: process.env["STRIPE_TAMPA_PUBLIC_KEY"] as string,
       };
     default:
       return {
@@ -84,16 +84,16 @@ export const getStripeWebhookSigningKey = (city: string): string => {
   switch (city) {
     /* istanbul ignore next */
     case "tampa":
-      return process.env.STRIPE_TAMPA_WEBHOOK_KEY as string;
+      return process.env["STRIPE_TAMPA_WEBHOOK_KEY"] as string;
     /* istanbul ignore next */
     case "orlando":
-      return process.env.STRIPE_ORLANDO_WEBHOOK_KEY as string;
+      return process.env["STRIPE_ORLANDO_WEBHOOK_KEY"] as string;
     /* istanbul ignore next */
     case "birmingham":
-      return process.env.STRIPE_BIRMINGHAM_WEBHOOK_KEY as string;
+      return process.env["STRIPE_BIRMINGHAM_WEBHOOK_KEY"] as string;
     /* istanbul ignore next */
     case "baltimore":
-      return process.env.STRIPE_BALTIMORE_WEBHOOK_KEY as string;
+      return process.env["STRIPE_BALTIMORE_WEBHOOK_KEY"] as string;
     default:
       return "";
   }
